a228ea078297f60ebe27bd9038cfd329123458e9
cool-delete <kk520149492@gmail.com>
2021-09-05 06:44:41 +0800

    转换3.0失败 原因推测为 1.setup执行太快了 还无法访问dom 2.放入onMouth的挂宅钩子实例化正常,但是setup里面返回了map对象 应用到子component里面 这时候的map对象为null,map是黑盒 无法应用proxy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,7 @@
       @startTracking="startTracking"
       @outTracking="outTracking"
       @goFenceSetting="isFenceSetting = !isFenceSetting"
+      ref="navgate"
     ></navgate>
     <controlPlayback
       v-if="isControlPlayback"
@@ -46,10 +47,13 @@ import Fence from "@m/aFence/Fence";
 // import ComplexCustomOverlay from "../module/ComplexCustomOverlay";
 import { debounce, cloneDeep } from "lodash";
 import carICon from "@/assets/car.png";
+import WorkerIsPolyIn from '@m/WorkerIsPolyIn?worker'
+import request from '@/until/request';
 import navgate from "@/components/navgate.vue";
 import FenceSetting from "@/components/FenceSetting.vue";
 import controlPlayback from "@/components/controlPlayback.vue";
-import { defineAsyncComponent, defineComponent } from "vue";
+import { defineAsyncComponent, defineComponent, ref, reactive, onMounted, } from "vue";
+import { ElNotification } from 'element-plus';
 import { http } from "@/until/request";
 export default defineComponent({
   //import引入的组件需要注入到对象中才能使用
@@ -61,13 +65,12 @@ export default defineComponent({
       () => import("@/components/historicalRecord.vue"),
     ),
   },
-  data() {
-    //这里存放数据
+  setup(props, contxt) {
+
     let cars: car[] = [{
       identificationCode: "HK01",
       superiorDepartments: "警务处",
       higherUnit: "广州巡逻",
-      state: "巡逻",
       trackPoints: Array(500)
         .fill(0)
         .map((_, i) => {
@@ -83,10 +86,9 @@ export default defineComponent({
       },
     },
     {
-      identificationCode: "粤A1236",
+      identificationCode: "HK02",
       superiorDepartments: "警务处",
       higherUnit: "广州巡逻",
-      state: "待机",
       trackPoints: [],
       position: {
         lng: 113.336251,
@@ -101,228 +103,269 @@ export default defineComponent({
       trackingPath: any[] = [],
       historyP: any = {},
       historyCar: any = {},
-      historyPLine: any = {}
-    return {
-      history,
-      historyP,
-      historyCar,
-      planToBounds,
-      trackingLine,
-      trackingPath,
-      historyPLine,
-      isToDisplayMapLS: false,
-      isFenceSetting: false,
-      isControlPlayback: false,
-      currentTrack: {
+      historyPLine: any = {},
+      map: {
+        setViewport: (_: Array<Object>) => {},
+        setCenter: (d: any) => {},
+        removeOverlay: (d: any) => {},
+        panTo: (d: any) => {},
+        getBounds: () => ({}),
+        addEventListener: (d: string, e: Function) => {},
+        removeEventListener: (d: string, e: Function) => {},
+        addOverlay: (_: any) => {},
+        centerAndZoom: (_: any, d: number) => {},
+        addControl: (_: any) => {},
+        enableScrollWheelZoom: (_: any) => {},
+
+      }, // 创建Map实例,
+      currentTrack = {
         name: '',
         history: [{
           time: Number,
           lng: Number,
           lat: Number,
         }]
-      }, cars,
-      map: {
-        setViewport: (_: Array<Object>) => { },
-        setCenter: (d: any) => { },
-        removeOverlay: (d: any) => { },
-        panTo: (d: any) => { },
-        getBounds: () => ({}),
-        addEventListener: (d: string, e: Function) => { },
-        removeEventListener: (d: string, e: Function) => { },
-        addOverlay: (_: any) => _
+      },
+      isToDisplayMapLS = false,
+      isFenceSetting = ref(false),
+      isControlPlayback = false,
+      currentRadius = 100,
+      alarmRange = 150
+
+
+    const testPoint = (p: any): boolean => {
+      return planToBounds.containsPoint(p)
+    },
+      FenceSetting = (): void => {
+        new Fence(map, { strokeColor: "blue" }, BMAP_DRAWING_POLYGON)
+      },
+      setBounds = (): void => {
+        planToBounds = map.getBounds()
+        let getNorthEast, getSouthWest
+        getNorthEast = getSouthWest = planToBounds.getCenter()
+        let { lng: swlng, lat: swlat } = planToBounds.getSouthWest() as { lng: number, lat: number }
+        let { lng: nelng, lat: nelat } = planToBounds.getNorthEast() as { lng: number, lat: number }
+        const zoom = 0.03, width = (nelng - swlng) * zoom,
+          height = (nelat - swlat) * zoom
+        swlng = swlng + width
+        swlat = swlat + height
+        nelng = nelng - width
+        nelat = nelat - height
+        getSouthWest.lng = swlng
+        getSouthWest.lat = swlat
+        getNorthEast.lng = nelng
+        getNorthEast.lat = nelat
+        planToBounds = new BMap.Bounds(getSouthWest, getNorthEast)
+      },
+      outTracking = () => {
+        map.removeOverlay(trackingLine)
+        trackingPath.length = 0
+        map.removeEventListener('moveend', setBounds
+        )
       },
 
 
-      currentRadius: 100,
-      alarmRange: 150,
-    };
-  },
-  //监听属性 类似于data概念
-  computed: {},
-  //监控data中的数据变化
-  watch: {},
-  //方法集合
-  methods: {
-    testPoint(p: any): boolean {
-      return this.planToBounds.containsPoint(p)
-    },
-    FenceSetting(): void {
-      new Fence(this.map, { strokeColor: "blue" }, BMAP_DRAWING_POLYGON)
-    },
-    setBounds(): void {
-      this.planToBounds = this.map.getBounds()
-      let getNorthEast, getSouthWest
-      getNorthEast = getSouthWest = this.planToBounds.getCenter()
-      let { lng: swlng, lat: swlat } = this.planToBounds.getSouthWest() as { lng: number, lat: number }
-      let { lng: nelng, lat: nelat } = this.planToBounds.getNorthEast() as { lng: number, lat: number }
-      const zoom = 0.03, width = (nelng - swlng) * zoom,
-        height = (nelat - swlat) * zoom
-      swlng = swlng + width
-      swlat = swlat + height
-      nelng = nelng - width
-      nelat = nelat - height
-      getSouthWest.lng = swlng
-      getSouthWest.lat = swlat
-      getNorthEast.lng = nelng
-      getNorthEast.lat = nelat
-      this.planToBounds = new BMap.Bounds(getSouthWest, getNorthEast)
-    },
-    outTracking() {
-      this.map.removeOverlay(this.trackingLine)
-      this.trackingPath.length = 0
-      this.map.removeEventListener('moveend', this.setBounds
-      )
-    },
-
-
-    startTracking(p: any) {
-      this.map.panTo(p)
-      this.trackingLine = new BMap.Polyline([p], { strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5 })
-      this.map.addOverlay(this.trackingLine)
-      this.setBounds()
-      this.map.addEventListener('moveend', this.setBounds
-      )
-    },
-    onTrackingView(pos: any, define: string) {
-      if (!this.testPoint(pos))
-        this.setBounds(), this.map.panTo(pos)
-    },
-    onPathView(pos: any, define: string) {
-      this.trackingPath.push(pos)
-      this.trackingLine.setPath(this.trackingPath)
-    },
-    outView() {
-      this.map.removeOverlay(this.historyPLine)
-      this.map.removeOverlay(this.historyCar)
-      this.isControlPlayback = false
-      this.isFenceSetting = false
-    },
-    movePoints(i: number) {
-      let that = this.history[i]
-      const { lng, lat } = that
-      this.historyP.lng = lng
-      this.historyP.lat = lat
-      // console.log(this.historyP);
-      this.historyCar.setPosition(this.historyP)
-      this.map.setCenter(this.historyP);
-    },
-    historyShows(history: history[]) {
-      this.outView()
-      this.history = history
-      console.log(history);
-      this.isControlPlayback = true
-      const arrP = history.map(h => {
-        let p = cloneDeep<history>(this.historyP)
-        p.lng = h.lng
-        p.lat = h.lat
-        return p
-      })
-      const pl = new BMap.Polyline(arrP, { strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5 })
-      let OneCar = new BMap.Marker(
-        arrP[0],
-        {
-          icon: new BMap.Icon(carICon, new BMap.Size(52, 26)),
-        }
-      );
-      let lebal = new BMap.Label(history[0].car, {
-        offset: new BMap.Size(10, -30),
-      });
-      OneCar.setLabel(lebal);
-      this.historyPLine = pl
-      this.historyCar = OneCar
-      this.map.addOverlay(pl)
-      this.map.addOverlay(OneCar)
-    },
-    showHistoryCar(car: string) {
-      this.isToDisplayMapLS = !this.isToDisplayMapLS
-      this.currentTrack.name = car
-    },
-    focusAll() {
-      this.map.setViewport(this.cars.map((car) => car.position!.icar!.point));
-    },
-    setView(pos: any, define: string) {
-      console.log(pos.car);
-      this.map.panTo(pos.icar.point);
-    },
-    initMap() {
-      // 百度地图API功能
-      let map = (this.map = new BMap.Map("allmap")); // 创建Map实例
-      const currentPoint = new BMap.Point(113.336251, 23.107998);
-      map.centerAndZoom(currentPoint, 14); // 初始化地图,设置中心点坐标和地图级别
-      //添加地图类型控件
-      this.historyP = new BMap.Point(113.336251, 23.107998)
-      map.addControl(
-        new (BMap as any).MapTypeControl({
-          mapTypes: [BMAP_NORMAL_MAP, BMAP_SATELLITE_MAP, BMAP_HYBRID_MAP],
+      startTracking = (p: any) => {
+        map.panTo(p)
+        trackingLine = new BMap.Polyline([p], { strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5 })
+        map.addOverlay(trackingLine)
+        setBounds()
+        map.addEventListener('moveend', setBounds
+        )
+      },
+      onTrackingView = (pos: any, define: string) => {
+        if (!testPoint(pos))
+          setBounds(), map.panTo(pos)
+      },
+      onPathView = (pos: any, define: string) => {
+        trackingPath.push(pos)
+        trackingLine.setPath(trackingPath)
+      },
+      outView = () => {
+        map.removeOverlay(historyPLine)
+        map.removeOverlay(historyCar)
+        isControlPlayback = false
+        isFenceSetting.value = false
+      },
+      movePoints = (i: number) => {
+        let that = history[i]
+        const { lng, lat } = that
+        historyP.lng = lng
+        historyP.lat = lat
+        // console.log(historyP);
+        historyCar.setPosition(historyP)
+        map.setCenter(historyP);
+      },
+      historyShows = (history: history[]) => {
+        outView()
+        history = history
+        console.log(history);
+        isControlPlayback = true
+        const arrP = history.map(h => {
+          let p = cloneDeep<history>(historyP)
+          p.lng = h.lng
+          p.lat = h.lat
+          return p
         })
-      );
-      //离线不支持
-      // map.setCurrentCity("北京"); // 设置地图显示的城市 此项是必须设置的
-      map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
-
-      let circle = new BMap.Circle(currentPoint, this.currentRadius, {
-        strokeColor: "blue",
-        strokeWeight: 2,
-        strokeOpacity: 0.4,
-      }); //创建圆
-      //绘制
-      let txt = "广州珠江帝景电子围栏 现有",
-        mouseoverTxt = txt + " " + 1 + "车辆";
-
-      const electronicFence = new ComplexCustomOverlay(
-        currentPoint,
-        "电子围栏",
-        mouseoverTxt
-      );
-      map.addOverlay(electronicFence);
-      map.addOverlay(circle);
-      this.move(this.cars)();
-      setTimeout(function () {
-        // layer.msg("鼠标滚轮缩放", { offset: "b" });
-      }, 1000);
-    },
-    move(car: car[]) {
-      return function () {
-        let t: NodeJS.Timeout,
-          o = 0;
-        car.forEach((c) => {
-          setInterval(async () => {
-            const p: car["position"] = (await http.post(`/db/query`, { fir_no: c.identificationCode }, {
-              headers: { 'content-type': 'application/json' }
-            }))["data"];
-            c.position.lng = p.lng;
-            c.position.lat = p.lat;
-            o++;
-          }, 10000);
-        })
-      }
-
-    },
-    initializesVehicleDisplay(cars: any[]) {
-      // offset = 0;
-      cars.forEach((car) => {
+        const pl = new BMap.Polyline(arrP, { strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5 })
         let OneCar = new BMap.Marker(
-          new BMap.Point(car.position.lng, car.position.lat),
+          arrP[0],
           {
             icon: new BMap.Icon(carICon, new BMap.Size(52, 26)),
           }
         );
-        let lebal = new BMap.Label(car.state, {
+        let lebal = new BMap.Label(history[0].car, {
           offset: new BMap.Size(10, -30),
         });
         OneCar.setLabel(lebal);
-        OneCar.setTitle(car.identificationCode);
-        car.position.icar = OneCar;
-        this.map.addOverlay(OneCar);
-      });
-    },
-  },
-  //生命周期 - 创建完成（可以访问当前this实例）
-  created() {
+        historyPLine = pl
+        historyCar = OneCar
+        map.addOverlay(pl)
+        map.addOverlay(OneCar)
+      },
+      showHistoryCar = (car: string) => {
+        isToDisplayMapLS = !isToDisplayMapLS
+        currentTrack.name = car
+      },
+      focusAll = () => {
+        map.setViewport(cars.map((car) => car.position!.icar!.point));
+      },
+      setView = (pos: any, define: string) => {
+        console.log(pos.car);
+        map.panTo(pos.icar.point);
+      },
+      initMap = () => {
+        // 百度地图API功能
+        map = (new BMap.Map("allmap"))
+        const currentPoint = new BMap.Point(113.336251, 23.107998);
+       setTimeout(()=>{
+          map.centerAndZoom(currentPoint, 14); // 初始化地图,设置中心点坐标和地图级别
+        //添加地图类型控件
+        historyP = new BMap.Point(113.336251, 23.107998)
+        map.addControl(
+          new (BMap as any).MapTypeControl({
+            mapTypes: [BMAP_NORMAL_MAP, BMAP_SATELLITE_MAP, BMAP_HYBRID_MAP],
+          })
+        );
+        //离线不支持
+        // map.setCurrentCity("北京"); // 设置地图显示的城市 此项是必须设置的
+        map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
+
+        let circle = new BMap.Circle(currentPoint, currentRadius, {
+          strokeColor: "blue",
+          strokeWeight: 2,
+          strokeOpacity: 0.4,
+        }); //创建圆
+        //绘制
+        let txt = "广州珠江帝景电子围栏 现有",
+          mouseoverTxt = txt + " " + 1 + "车辆";
+
+        const electronicFence = new ComplexCustomOverlay(
+          currentPoint,
+          "电子围栏",
+          mouseoverTxt
+        );
+        map.addOverlay(electronicFence);
+        map.addOverlay(circle);
+        move(cars)();
+        setTimeout(function () {
+          // layer.msg("鼠标滚轮缩放", { offset: "b" });
+        }, 1000);
+       },0)
+      },
+      move = (car: car[]) => {
+        const that = this
+        return function () {
+          let t: NodeJS.Timeout,
+            o = 0;
+          car.forEach((c) => {
+            setInterval(async () => {
+              const p: car["position"] = (await http.post(`/db/query`, { fir_no: c.identificationCode }, {
+                headers: { 'content-type': 'application/json' }
+              }))["data"];
+              c.position.lng = p.lng;
+              c.position.lat = p.lat;
+              o++;
+            }, 10000);
+          })
+          calcIsPolyIn(car)
+        }
+
+      },
+      calcIsPolyIn = (car: car[]) => {
+        const carsB: any = []
+        setInterval(async () => {
+          carsB.length = 0
+          cars.forEach((c) => {
+            carsB.push({ id: c.identificationCode, lng: c.position.lng, lat: c.position.lat })
+          })
+          const worke = new WorkerIsPolyIn()
+          carsB.state = -1
+          carsB.add = request.baseURL
+          worke.postMessage(carsB)
+
+
+          worke.onmessage = (e: any) => {
+            if (e.data.state) {
+              worke.postMessage((carsB.state = 3, carsB))
+              return
+            }
+
+            pointOutAlert(e.data)
+          }
+        }, 15000);
+
+      },
+      pointOutAlert = (car: car[]) => {
+
+        console.log('警报', car);
+        const notifonclick = (id: string) => {
+          return (e: PointerEvent) => {
+            if ((e.target as HTMLElement)!.className == "track") {
+              const out = cars.filter(car => car.identificationCode === id)
+              // @ts-ignore
+              navgate.setup(props, contxt).tracking(out[0].position)
+            }
+          }
+        }
+
+        const outNoif = ElNotification({
+          title: '错误',
+          // duration: 0,
+          // @ts-ignore  
+          onclick: notifonclick(car[0].identificationCode),
+          dangerouslyUseHTMLString: true,
+          message: `<i class="el-notification__icon el-icon-error mynotify"></i><span>发现${car[0].identificationCode}出界 可点击</span>
+        <span class='track' >跟踪</span>
+    
+        `
+        })
+
+
+        // 
+      },
+      initializesVehicleDisplay = (cars: any[]) => {
+        // offset = 0;
+        cars.forEach((car) => {
+          let OneCar = new BMap.Marker(
+            new BMap.Point(car.position.lng, car.position.lat),
+            {
+              icon: new BMap.Icon(carICon, new BMap.Size(52, 26)),
+            }
+          );
+          // let lebal = new BMap.Label(car.state, {
+          //   offset: new BMap.Size(10, -30),
+          // });
+          // OneCar.setLabel(lebal);
+          OneCar.setTitle(car.identificationCode);
+          car.position.icar = OneCar;
+          map.addOverlay(OneCar);
+        });
+      }
     let draw = (point: car['position']) =>
       point.icar.setPosition!(new BMap.Point(point.lng, point.lat))
-    
-    this.cars.forEach((car) => {
+
+    cars.forEach((car) => {
       let handle = {
         set: function (obj: car['position'], prop: PropertyKey, value: any) {
           let res = Reflect.set(obj, prop, value);
@@ -333,20 +376,43 @@ export default defineComponent({
       };
       (car.position as object) = new Proxy(car.position, handle);
     });
-  },
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {
-    this.initMap();
-    this.initializesVehicleDisplay(this.cars);
-  },
-  beforeCreate() { }, //生命周期 - 创建之前
-  beforeMount() { }, //生命周期 - 挂载之前
-  beforeUpdate() { }, //生命周期 - 更新之前
-  updated() { }, //生命周期 - 更新之后
-  beforeUnmount() { }, //生命周期 - 销毁之前
-  unmounted() { }, //生命周期 - 销毁完成
-  activated() { }, //如果页面有keep-alive缓存功能，这个函数会触发
+    initMap();
+    initializesVehicleDisplay(cars);
+
+    return {
+      navgate: ref(null),
+      history,
+      historyP,
+      historyCar,
+      planToBounds,
+      trackingLine,
+      trackingPath,
+      historyPLine,
+      isToDisplayMapLS,
+      isFenceSetting,
+      isControlPlayback,
+      currentTrack, cars,
+      // @ts-ignore
+      map,
+      currentRadius,
+      alarmRange,
+      focusAll,
+      setView,
+      showHistoryCar,
+      onTrackingView,
+
+
+      onPathView,
+      startTracking,
+      outTracking,
+      movePoints,
+      outView,
+      historyShows,
+    }
+  }
+
 })
+
 </script>
 <style  scoped>
 /* @import url('../../lib/DrawingManager_min.css'); 引入公共css类 */
